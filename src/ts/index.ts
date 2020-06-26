f5c237c811f46db1d532b7d6bc7123d698cbcf64
amarolahy <ramah.samuel@gmail.com>
2020-06-26 11:22:04 +0300

    Script: make the clock work

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,19 @@
 import '../sass/index.scss';
+
+const hoursHand = document.querySelector<HTMLDivElement>('#hours');
+const minutesHand = document.querySelector<HTMLDivElement>('#minutes');
+const secondsHand = document.querySelector<HTMLDivElement>('#seconds');
+
+const setTime = () => {
+  const now = new Date();
+
+  const s = now.getSeconds();
+  const m = now.getMinutes();
+  const h = now.getHours();
+
+  hoursHand.style.transform = `rotate(${(h / 12) * 360}deg)`;
+  minutesHand.style.transform = `rotate(${(m / 60) * 360}deg)`;
+  secondsHand.style.transform = `rotate(${(s / 60) * 360}deg)`;
+}
+
+setInterval(setTime, 1000);
